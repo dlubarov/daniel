61b7ca2d401032b5b13fe8024261eaf670425f11
Daniel Lubarov <dlubarov@squareup.com>
2013-02-24 14:19:30 -0800

    Move version handler to shared web code.

---
+++ web/src/daniel/web/http/server/util/VersionHandler.java
@@ -0,0 +1,22 @@
+package daniel.web.http.server.util;
+
+import daniel.web.http.HttpRequest;
+import daniel.web.http.HttpResponse;
+import daniel.web.http.server.Handler;
+
+public final class VersionHandler implements Handler {
+  public static final VersionHandler singleton = new VersionHandler();
+
+  private VersionHandler() {}
+
+  @Override
+  public HttpResponse handle(HttpRequest request) {
+    String location = "https://github.com/dlubarov/daniel/commit/" + getVersion();
+    return HttpResponseFactory.redirectSameMethod(location);
+  }
+
+  private static String getVersion() {
+    String version = VersionHandler.class.getPackage().getImplementationVersion();
+    return version != null ? version : "unknown";
+  }
+}
