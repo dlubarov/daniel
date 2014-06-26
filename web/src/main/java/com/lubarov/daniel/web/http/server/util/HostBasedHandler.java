ea4eb986f109027e6ce25f92ff72431f72983fea
Daniel Lubarov <daniel@lubarov.com>
2014-06-26 14:55:35 -0700

    Quick HostBasedHandler fix

--- web/src/main/java/com/lubarov/daniel/web/http/server/util/HostBasedHandler.java
+++ web/src/main/java/com/lubarov/daniel/web/http/server/util/HostBasedHandler.java
@@ -1,18 +1,20 @@
 package com.lubarov.daniel.web.http.server.util;
 
-import com.lubarov.daniel.data.dictionary.ImmutableDictionary;
-import com.lubarov.daniel.data.dictionary.MutableHashDictionary;
+import com.lubarov.daniel.data.dictionary.KeyValuePair;
+import com.lubarov.daniel.data.sequence.ImmutableSequence;
+import com.lubarov.daniel.data.stack.DynamicArray;
+import com.lubarov.daniel.data.stack.MutableStack;
 import com.lubarov.daniel.web.http.HttpRequest;
 import com.lubarov.daniel.web.http.HttpResponse;
 import com.lubarov.daniel.web.http.server.Handler;
 
 public final class HostBasedHandler implements Handler {
   public static final class Builder {
-    private final MutableHashDictionary<String, Handler>
-        handlersByHost = MutableHashDictionary.create();
+    private final MutableStack<KeyValuePair<String, Handler>> handlersByHostRegex =
+        DynamicArray.create();
 
     public Builder addHandlerForHost(String hostRegex, Handler handler) {
-      handlersByHost.put(hostRegex, handler);
+      handlersByHostRegex.pushBack(new KeyValuePair<>(hostRegex, handler));
       return this;
     }
 
@@ -21,18 +23,18 @@ public final class HostBasedHandler implements Handler {
     }
   }
 
-  private final ImmutableDictionary<String, Handler> handlersByHost;
+  private final ImmutableSequence<KeyValuePair<String, Handler>> handlersByHostRegex;
 
   private HostBasedHandler(Builder builder) {
-    handlersByHost = builder.handlersByHost.toImmutable();
+    handlersByHostRegex = builder.handlersByHostRegex.toImmutable();
   }
 
   @Override
   public HttpResponse handle(HttpRequest request) {
     String host = request.getHost().toLowerCase();
-    for (String hostRegex : handlersByHost.getKeys())
-      if (host.matches(hostRegex))
-        return handlersByHost.getValue(hostRegex).handle(request);
+    for (KeyValuePair<String, Handler> hostRegexAndHandler : handlersByHostRegex)
+      if (host.matches(hostRegexAndHandler.getKey()))
+        return hostRegexAndHandler.getValue().handle(request);
     throw new IllegalArgumentException(String.format("No handler for %s.", host));
   }
 }
