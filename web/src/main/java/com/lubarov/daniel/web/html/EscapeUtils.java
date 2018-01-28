b417f5802375cb11f318b71bc615fc08a21e4251
Daniel Lubarov <daniel@lubarov.com>
2018-01-28 10:42:54 -0800

    Encode pipe character

--- web/src/main/java/com/lubarov/daniel/web/html/EscapeUtils.java
+++ web/src/main/java/com/lubarov/daniel/web/html/EscapeUtils.java
@@ -27,6 +27,9 @@ public final class EscapeUtils {
         case '/':
           sb.append("&#x2F;");
           break;
+        case '|':
+          sb.append("&#x7C;");
+          break;
         default:
           sb.append(c);
       }
